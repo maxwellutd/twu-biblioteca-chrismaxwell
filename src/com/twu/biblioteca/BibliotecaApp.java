67093cef5ea6857b111cc0bf381a979398ba57a9
maxwellutd <themailman2>
2015-01-06 17:37:23 +1100

    testing it in intellij

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,6 +5,7 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("time for changes");
+        System.out.println("testing if github updates");
     }
 
 
